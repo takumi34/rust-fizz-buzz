6b038ae8a44fbbd2fb4207afeb539c72465c489f
takumi34 <1shiranui@gmail.com>
2020-02-25 02:28:56 +0900

    add fizzbuzz

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    for i in 1..100 {
+        if i % 3 == 0 {
+            println!("{} :fizz",i);
+        } else if i % 5 == 0 {
+            println!("{} :buzz", i);
+        } else if i % 15 == 0 {
+            println!("{} :fizz buzz", i);
+        }
+    }
+}
